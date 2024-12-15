b77671abc63de3e7d8134721313162b11490ede8
Do Thai Hoc <78217280+hocvn@users.noreply.github.com>
2024-12-15 20:38:32 +0700

    #2 - Change some attribute of product model based on database

--- server/models/Product.js
+++ server/models/Product.js
@@ -1,7 +1,7 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db');
 
-const Product = sequelize.define('Product', {
+const Product = sequelize.define('product', {
     id: {
         type: DataTypes.INTEGER,
         primaryKey: true,
@@ -11,34 +11,47 @@ const Product = sequelize.define('Product', {
         type: DataTypes.TEXT,
         allowNull: false,
     },
+    url_key: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+    },
     images: {
         type: DataTypes.JSONB,
-        allowNull: false,
-    },
-    category_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-    },
-    rating: {
-        type: DataTypes.FLOAT,
-        allowNull: false,
-    },
-    discount_rate: {
-        type: DataTypes.FLOAT,
-        allowNull: false,
-        defaultValue: 0,
-    },
-    original_price: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-    },
-    status: {
-        type: DataTypes.TEXT,
         allowNull: false,
     },
     short_description: {
         type: DataTypes.TEXT,
         allowNull: true,
+    },
+    category_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    category_name: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+    },
+    original_price: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+    },
+    price: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+    },
+    rating_average: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+        defaultValue: 0,
+    },
+    discount_rate: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+    },
+    inventory_status: {
+        type: DataTypes.TEXT,
+        allowNull: false,
     },
     description: {
         type: DataTypes.TEXT,
@@ -48,19 +61,22 @@ const Product = sequelize.define('Product', {
         type: DataTypes.TEXT,
         allowNull: true,
     },
-    quantity: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        defaultValue: 0,
+    video_url: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+    },
+    specifications: {
+        type: DataTypes.TEXT,
+        allowNull: true,
     },
     quantity_sold: {
         type: DataTypes.INTEGER,
         allowNull: true,
         defaultValue: 0,
     },
-    specifications: {
-        type: DataTypes.TEXT,
-        allowNull: true,
+    qty: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
     },
     current_seller: {
         type: DataTypes.JSONB,

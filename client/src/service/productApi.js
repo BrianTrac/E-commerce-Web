e8d966dcb49860a9802fdfcbc5f86394fd9310fb
22120163 <22120163@student.hcmus.edu.vn>
2025-01-02 12:29:04 +0700

    Feature: add ProductReview and ProductCarousel components; remove unused slick dependency

--- client/src/service/productApi.js
+++ client/src/service/productApi.js
@@ -51,7 +51,6 @@ const fetchProductByCategory = async (
 
 const fetchProductById = async (id) => {
   try {
-    debugger
     const response = await axios.get(`api/products/${id}`);
     return response.data;
   } catch (error) {
@@ -60,43 +59,27 @@ const fetchProductById = async (id) => {
   }
 }
 
-// const API_URL = 'http://localhost:4000/api/admin';
+const fetchProductReviews = async (id) => {
+  try {
+    const response = await fetch(`https://tiki.vn/api/v2/reviews?product_id=${id}`);
 
-// const sellerApi = {
-//   getProducts: async (sellerId, params = {}) => {
-//     const { page = 1, limit = 10, search = '' } = params;
-//     const queryParams = new URLSearchParams({
-//       page,
-//       limit,
-//       search
-//     }).toString();
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
 
-//     const response = await fetch(`${API_URL}/seller/${sellerId}/products?${queryParams}`);
+    const data = await response.json(); 
+    return data; 
+  } catch (error) {
+    console.error('Failed to fetch product reviews', error);
+    throw error; 
+  }
+};
 
-//     if (!response.ok) {
-//       throw new Error(response.statusText);
-//     }
-
-//     return await response.json();
-//   },
-
-//   deleteProduct: async (sellerId, productId) => {
-//     const response = await fetch(`${API_URL}/seller/${sellerId}/products/${productId}`, {
-//       method: 'DELETE'
-//     });
-
-//     if (!response.ok) {
-//       throw new Error('Failed to delete product');
-//     }
-
-//     return await response.json();
-//   }
-// };
 
 export {
   getTopDeals,
   getFlashSale,
   fetchProductByCategory,
   fetchProductById,
-  // sellerApi
+  fetchProductReviews,
 };

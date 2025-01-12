ce928ca0d52a78fed61f059eff588cc0716e0bfa
22120163 <22120163@student.hcmus.edu.vn>
2025-01-12 09:30:24 +0700

    feat: implement role-based access control for admin routes

--- server/routes/admin/index.route.js
+++ server/routes/admin/index.route.js
@@ -1,8 +1,10 @@
-const express = require('express');
-const verifyJWT = require('../../middlewares/verifyJWT.middleware');
+const ROLES_LIST = require('../../config/roles_list');
+const verifyRoles = require('../../middlewares/verifyRoles.middleware');
+
 
 const adminRoutes = (app) => {
     // Routes
+    app.use(verifyRoles(ROLES_LIST.Admin));
     app.use('/api/admin/seller', require('./manageSeller.route'));
     app.use('/api/admin/user', require('./manageUser.route'));
     app.use('/api/admin/products', require('./manageProduct.route'));

e068b1531348adb77c0a9915c0550bd926c1cd3e
kiin21 <139786046+kiin21@users.noreply.github.com>
2025-01-12 14:09:01 +0700

    Update index.route.js

--- server/routes/admin/index.route.js
+++ server/routes/admin/index.route.js
@@ -4,11 +4,10 @@ const verifyRoles = require('../../middlewares/verifyRoles.middleware');
 
 const adminRoutes = (app) => {
     // Routes
-    app.use(verifyRoles(ROLES_LIST.Admin));
-    app.use('/api/admin/seller', require('./manageSeller.route'));
-    app.use('/api/admin/user', require('./manageUser.route'));
-    app.use('/api/admin/products', require('./manageProduct.route'));
-    app.use('/api/admin/statistic', require('./salesStatistics.route'));
+    app.use('/api/admin/seller', verifyRoles(ROLES_LIST.Admin), require('./manageSeller.route'));
+    app.use('/api/admin/user', verifyRoles(ROLES_LIST.Admin), require('./manageUser.route'));
+    app.use('/api/admin/products', verifyRoles(ROLES_LIST.Admin), require('./manageProduct.route'));
+    app.use('/api/admin/statistic', verifyRoles(ROLES_LIST.Admin), require('./salesStatistics.route'));
 }
 
 module.exports = adminRoutes;
